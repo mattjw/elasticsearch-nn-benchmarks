add7a0c6f40251221817cb779b91eaac1c91e05c
Matt J Williams <matt-williams@cookpad.com>
2019-10-04 16:05:22 +0100

    Implement query performance checking.

--- src/experiments.py
+++ src/experiments.py
@@ -3,7 +3,9 @@ import re
 from typing import List
 
 from populate_index import populate_vectors
+from query_index import test_query_fcs, test_query_dense
 
+QUERY_EXPERIMENT_NUM_REPEATS = 1
 TEST_CASES_DIR = "./data/slices/"
 
 
@@ -24,13 +26,40 @@ def test_cases(test_cases_dir=TEST_CASES_DIR) -> List[dict]:
 
 def run_experiments():
     for case in test_cases():
-        print(case)
-        print(populate_vectors(case["fpath"], "fcs"))
-        print(populate_vectors(case["fpath"], "dense"))
+        test_case_results = {"test_case": case}
+
+        # # FCS
+        record = {}
+        record["populate_time_secs"] = populate_vectors(case["fpath"], "fcs")
+        record["populate_time_avg_secs"] = record["populate_time_secs"] / case["num_vectors"]
+        record["populate_time_ips"] = 1.0 / record["populate_time_avg_secs"]  # insertions per second
+
+        record["query_time_avg_secs"] = test_query_fcs(case["fpath"], QUERY_EXPERIMENT_NUM_REPEATS)
+        record["query_time_num_repeats"] = QUERY_EXPERIMENT_NUM_REPEATS
+        record["query_time_qps"] = 1.0 / record["query_time_avg_secs"]  # queries per second
+
+        test_case_results["fcs"] = record
+
+        # dense
+        record = {}
+        record["populate_time_secs"] = populate_vectors(case["fpath"], "dense")
+        record["populate_time_avg_secs"] = record["populate_time_secs"] / case["num_vectors"]
+        record["populate_time_ips"] = 1.0 / record["populate_time_avg_secs"]  # insertions per second
+
+        record["query_time_avg_secs"] = test_query_dense(case["fpath"], QUERY_EXPERIMENT_NUM_REPEATS)
+        record["query_time_num_repeats"] = QUERY_EXPERIMENT_NUM_REPEATS
+        record["query_time_qps"] = 1.0 / record["query_time_avg_secs"]  # queries per second
+
+        test_case_results["dense"] = record
+
+        print(
+            "{}\n  fcs:   {}\n  dense: {}".format(
+                test_case_results["test_case"], test_case_results["fcs"], test_case_results["dense"]))
 
 
 def main():
     run_experiments()
+
 
 if __name__ == "__main__":
     main()
---
+++ src/query_index.py
@@ -0,0 +1,109 @@
+import random
+import time
+from typing import List, Tuple
+
+from constants import INDEX_NAME
+from populate_index import load_fcs_vectors, FCS_VECTOR_FIELD, DENSE_VECTOR_FIELD, TERM_FIELD, load_dense_vectors
+from utils import client
+
+
+def query_nearest_vectors_fcs(vector_base64: str) -> List[Tuple[str, float]]:
+    """Find nearest terms to a given a vector, returning (term, score) pairs."""
+    hits = client().search(
+        index=INDEX_NAME,
+        body={
+            "query": {
+                "function_score": {
+                    "boost_mode": "replace",
+                    "functions": [{
+                        "script_score": {
+                            "script": {
+                                "source": "staysense",
+                                "lang": "fast_cosine",
+                                "params": {
+                                    "field": FCS_VECTOR_FIELD,
+                                    "cosine": True,
+                                    "encoded_vector": vector_base64
+                                }
+                            }
+                        }
+                    }]
+                }
+            }
+        },
+        _source=[TERM_FIELD]
+    )
+    neighbours = list((str(hit["_source"][TERM_FIELD]), float(hit["_score"])) for hit in hits["hits"]["hits"])
+    print(neighbours)  # ~
+    return neighbours
+
+
+def test_query_fcs(fpath, num_repeats) -> float:
+    """Get query run time measurements, with random query vector selection, returning average run time (seconds)."""
+    vectors = load_fcs_vectors(fpath)
+    t0 = time.time()
+    for _ in range(num_repeats):
+        vector = random.choice(vectors)
+        vector = vectors[0]  # ~
+        query_nearest_vectors_fcs(vector["vector"])
+    return (time.time() - t0) / num_repeats
+
+
+def query_nearest_vectors_dense(vector: List[float]) -> List[Tuple[str, float]]:
+    """Find nearest terms to a given a vector, returning (term, score) pairs."""
+
+    # note thatelasticsearch must have scores >= 0
+    # elastic's `cosineSimilarity` func retunrns in range -1 to 1
+
+    hits = client().search(
+        index=INDEX_NAME,
+        body={
+            "query": {
+                "function_score": {
+                    "boost_mode": "replace",
+                    "functions": [{
+                        "script_score": {
+                            "script": {
+                                "source": f"(cosineSimilarity(params.query_vector, doc['{DENSE_VECTOR_FIELD}']) + 1.0) / 2.0",
+                                "params": {"query_vector": vector}
+                            }
+                        }
+                    }]
+                }
+            }
+        },
+        _source=[TERM_FIELD]
+    )
+
+    # REMOVE ME: alternative way of scoring
+
+    # hits = client().search(
+    #     index=INDEX_NAME,
+    #     body={
+    #         "query": {
+    #             "script_score": {
+    #                 "query": {"match_all": {}},
+    #                 "script": {
+    #                     "source": f"(cosineSimilarity(params.query_vector, doc['{DENSE_VECTOR_FIELD}']) + 1.0) / 2.0",
+    #                     "params": {"query_vector": vector}
+    #                 }
+    #             }
+    #         }
+    #     },
+    #     _source=[TERM_FIELD]
+    # )
+
+    neighbours = list((str(hit["_source"][TERM_FIELD]), float(hit["_score"])) for hit in hits["hits"]["hits"])
+    print(neighbours)  #~
+    return neighbours
+
+
+def test_query_dense(fpath, num_repeats) -> float:
+    """Get query run time measurements, with random query vector selection, returning average run time (seconds)."""
+    vectors = load_dense_vectors(fpath)
+    t0 = time.time()
+    for _ in range(num_repeats):
+        vector = random.choice(vectors)
+        vector = vectors[0]  #~
+        query_nearest_vectors_dense(vector["vector"])
+    return (time.time() - t0) / num_repeats
